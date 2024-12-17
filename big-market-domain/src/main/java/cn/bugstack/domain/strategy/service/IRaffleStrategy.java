eedfab88aebb9c6088684f50c2b74482bd1c999d
chen <chenxjtu0129@163.com>
2024-12-18 01:09:45 +0800

    前置规则过滤

---
+++ big-market-domain/src/main/java/cn/bugstack/domain/strategy/service/IRaffleStrategy.java
@@ -0,0 +1,10 @@
+package cn.bugstack.domain.strategy.service;
+
+import cn.bugstack.domain.strategy.model.entity.RaffleAwardEntity;
+import cn.bugstack.domain.strategy.model.entity.RaffleFactorEntity;
+
+public interface IRaffleStrategy {
+
+    RaffleAwardEntity performRaffle(RaffleFactorEntity raffleFactorEntity);
+
+}
